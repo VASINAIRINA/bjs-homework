fb5c7367c5654bff14c9bae44a75fa0734ddef1a
Ирина Васина <59170484+VASINAIRINA@users.noreply.github.com>
2020-01-02 17:31:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let myName = "Ирина";
+let algebra = 5;
+let geography = 4;
+let physics = 4; 
+let ochenka;
+ochenka = (algebra + geography + physics) / 3;
+let message = (`Привет, мир! Меня зовут ${myName}`);
+console.log(message);
+console.log(`Средний балл равен ${ochenka}`);
